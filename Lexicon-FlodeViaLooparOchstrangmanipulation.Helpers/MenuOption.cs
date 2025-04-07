73db7199cb3f98b4499d7f67524da544ab1797a9
albertito1985 <anulo@live.se>
2025-04-07 13:02:00 +0200

    MenuOption with properties

--- Lexicon-FlodeViaLooparOchstrangmanipulation.Helpers/MenuOption.cs
+++ Lexicon-FlodeViaLooparOchstrangmanipulation.Helpers/MenuOption.cs
@@ -7,16 +7,23 @@ using System.Threading.Tasks;
 
 namespace Lexicon_FlodeViaLooparOchStrangmanipulation
 {
-    internal class MenuOption
+    public class MenuOption
     {
         static int inputCount = 0;
-        public int command;
-        public string description;
+        public delegate void HandlerFunction();
+        //public int command;
+        //public string description;
+        //public HandlerFunction handler;
 
-        public MenuOption(string inputDescription)
+        public int Command { get; set; }
+        public string Description{ get; set; }
+        public HandlerFunction Handler { get; set; }
+
+        public MenuOption(string inputDescription, HandlerFunction inputFunction)
         {
-            command = inputCount;
-            description = inputDescription;
+            Command = inputCount;
+            Description = inputDescription;
+            Handler = inputFunction;
             inputCount++;
         }
 

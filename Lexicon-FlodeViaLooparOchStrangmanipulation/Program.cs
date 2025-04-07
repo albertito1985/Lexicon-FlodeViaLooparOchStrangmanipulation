f686cb124d98486106b1d3042df7d0013cd1bb35
albertito1985 <anulo@live.se>
2025-04-07 09:54:34 +0200

    Without commented functions

--- Lexicon-FlodeViaLooparOchStrangmanipulation/Program.cs
+++ Lexicon-FlodeViaLooparOchStrangmanipulation/Program.cs
@@ -7,24 +7,15 @@ namespace Lexicon_FlodeViaLooparOchStrangmanipulation
         static void Main(string[] args)
         {
             int choice;
-            bool validatedInput = false;
+            bool exit = false;
             Menu mainMenu = new Menu();
 
             do
             {
                 mainMenu.ShowMenu();
                 choice = mainMenu.PromptOptionChoice();
-                validatedInput = mainMenu.ValidateAndExecuteOption(choice);
-            } while (!validatedInput);
-
-
-            //bool running = true;
-
-            //do
-            //{
-            //    MenuOption option1 = new MenuOption("something");
-
-            //} while (!running);
+                exit = mainMenu.ValidateAndExecuteOption(choice);
+            } while (!exit);
         }
     }
 }
